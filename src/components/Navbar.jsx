f1162bedf57565a00ec6a733924b3a5051e08229
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-05-09 18:31:20 +0300

    modified:   src/components/Navbar.jsx 	new file:   src/widgets/Logo.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,13 @@ import { useEffect, useState } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { auth } from '../auth/config/firebase-config';
+import Logo from '../widgets/Logo';
 
 const Navbar = ({ isDarkTheme, onProfileClick, themeSwitch }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [authCheckCompleted, setAuthCheckCompleted] = useState(false);
   const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const toggleProfileDropdown = () => setIsProfileDropdownOpen(!isProfileDropdownOpen);
   const [userPhotoURL, setUserPhotoURL] = useState('');
   const [userEmail, setUserEmail] = useState(''); // Store user's email
@@ -102,12 +103,8 @@ const Navbar = ({ isDarkTheme, onProfileClick, themeSwitch }) => {
             </button>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <a href='/' className="flex flex-shrink-0 items-center">
-              <img
-                className="h-12 w-auto"
-                src="./static/img/echo_1.png"
-                alt="Jervisecho"
-              />
+            <a href="/" className="flex flex-shrink-0 items-center">
+              <Logo />
             </a>
             {authCheckCompleted && !isAuthenticated ? (
               <div className="hidden sm:block sm:ml-6">
@@ -117,43 +114,41 @@ const Navbar = ({ isDarkTheme, onProfileClick, themeSwitch }) => {
                 </div>
               </div>
             ) : (
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                <Link
-                  to="/"
-                  className={`${
-                    location.pathname === '/' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                  } rounded-md px-3 py-2 text-sm font-medium`}
-                  aria-current={location.pathname === '/' ? 'page' : undefined}
-                >
-                  Dashboard
-                </Link>
-                <Link
-                  to="/models"
-                  className={`${
-                    location.pathname === '/models' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                  } rounded-md px-3 py-2 text-sm font-medium`}
-                  aria-current={location.pathname === '/models' ? 'page' : undefined}
-                >
-                  Models
-                </Link>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Community
-                </a>
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                >
-                  Pricing
-                </a>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+                  <Link
+                    to="/"
+                    className={`${location.pathname === '/' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+                      } rounded-md px-3 py-2 text-sm font-medium`}
+                    aria-current={location.pathname === '/' ? 'page' : undefined}
+                  >
+                    Dashboard
+                  </Link>
+                  <Link
+                    to="/models"
+                    className={`${location.pathname === '/models' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+                      } rounded-md px-3 py-2 text-sm font-medium`}
+                    aria-current={location.pathname === '/models' ? 'page' : undefined}
+                  >
+                    Models
+                  </Link>
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    Community
+                  </a>
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    Pricing
+                  </a>
+                </div>
               </div>
-            </div>
             )}
-            </div>
+          </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             {/* Theme Switch Button */}
             <div onClick={themeSwitch} className="cursor-pointer">
@@ -164,104 +159,104 @@ const Navbar = ({ isDarkTheme, onProfileClick, themeSwitch }) => {
               )}
             </div>
             {/* Profile dropdown */}
-          {isAuthenticated && (
-            <div className="relative ml-3">
-            <div>
-  <button
-    type="button"
-    className="p-2 relative flex items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-    id="user-menu-button"
-    aria-expanded="false"
-    aria-haspopup="true"
-    onClick={toggleProfileDropdown}
-  >
-    <span className="absolute -inset-1.5" />
-    <span className="sr-only">Open user menu</span>
-    {/* User's Profile Image */}
-    <img
-      className="h-8 w-8 rounded-full"
-      src={userPhotoURL || "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"} // Default image URL as fallback
-      alt="User Profile"
-    />
-    {/* Optionally display user's name next to the image */}
-    <span className="ml-2 text-white hidden sm:inline-block">{userName || "User"}</span>
-  </button>
-</div>
-              {isProfileDropdownOpen && (
-                <div
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="user-menu-button"
-                  tabIndex={-1}
-                >
-                  {/* Active: "bg-gray-100", Not Active: "" */}
-                  <div onClick={onProfileClick} className="cursor-pointer
-                    block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex={-1}
-                    id="user-menu-item-0"
+            {isAuthenticated && (
+              <div className="relative ml-3">
+                <div>
+                  <button
+                    type="button"
+                    className="p-2 relative flex items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    aria-haspopup="true"
+                    onClick={toggleProfileDropdown}
                   >
-                    Profile
-                  </div>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex={-1}
-                    id="user-menu-item-1"
-                  >
-                    Settings
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabIndex={-1}
-                    id="user-menu-item-2"
-                    onClick={handleSignOut}
-                  >
-                    Sign out
-                  </a>
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">Open user menu</span>
+                    {/* User's Profile Image */}
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src={userPhotoURL || "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"} // Default image URL as fallback
+                      alt="User Profile"
+                    />
+                    {/* Optionally display user's name next to the image */}
+                    <span className="ml-2 text-white hidden sm:inline-block">{userName || "User"}</span>
+                  </button>
                 </div>
-              )}
-            </div>
-          )}
+                {isProfileDropdownOpen && (
+                  <div
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu-button"
+                    tabIndex={-1}
+                  >
+                    {/* Active: "bg-gray-100", Not Active: "" */}
+                    <div onClick={onProfileClick} className="cursor-pointer
+                    block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="user-menu-item-0"
+                    >
+                      Profile
+                    </div>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="user-menu-item-1"
+                    >
+                      Settings
+                    </a>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="user-menu-item-2"
+                      onClick={handleSignOut}
+                    >
+                      Sign out
+                    </a>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         </div>
       </div>
       {/* Mobile menu, show/hide based on menu state. */}
       {isMobileMenuOpen && (
         <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-          <a
-            href="/"
-            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-            aria-current="page"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Models
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Community
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          >
-            Pricing
-          </a>
-        </div>
-      </div>)}
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
+            <a
+              href="/"
+              className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+              aria-current="page"
+            >
+              Dashboard
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Models
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Community
+            </a>
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            >
+              Pricing
+            </a>
+          </div>
+        </div>)}
     </nav>
   );
 };
